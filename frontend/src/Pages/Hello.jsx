97f5276835e55b14a89f4a4e2d9addf0f886d3bf
anmolmathur5198 <mathur.anmol@dotsquares.com>
2023-06-23 12:12:45 +0530

    hello.jsx added

---
+++ frontend/src/Pages/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>This is a checking point og git hub....</h1>
+        </div>
+
+    )
+}
+
+export default Hello
