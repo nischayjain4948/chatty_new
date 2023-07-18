540c83f1dff50a702d620d3ebb526f00e726344d
Nischay jain <nischayjain4948@gmail.com>
2023-07-18 23:58:32 +0530

    chatRoutes are completed AF!

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -5,7 +5,7 @@ const chatModel = mongoose.Schema({
     isGroupChat: { type: Boolean, default: false },
     users: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: User
+        ref: "User"
     }],
     latestMessage: {
         type: mongoose.Schema.Types.ObjectId,
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,4 +1,5 @@
 const { protect } = require('../middleware/authMiddleware');
+const {accessChat, fetchChats, createGroupChat, renameGroup, addToGroup,removeFromGroup} = require("../controllers/chatController")
 
 const router = require('express').Router();
 
@@ -11,8 +12,9 @@ router.route('/').post(protect, accessChat);
 router.route('/').get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
 router.route('/rename').put(protect, renameGroup);
-router.route("/groupremove").put(protect, removeFromGroup);
 router.route("/groupadd").put(protect, addToGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
+
 
 
 module.exports = router;
